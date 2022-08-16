f69c4952e56986812834da435f8bb532201ad74b
Sihui He <sardarbinarytree@gmail.com>
2022-08-16 19:20:30 -0400

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Calendar from "./components/Calendar";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
+import VideoCall from "./components/VideoCall";
 
 function App() {
   return (
@@ -11,10 +12,12 @@ function App() {
           <Link to="/"> Home </Link>
           <Link to="/profile"> Profile</Link>
           <Link to="/login"> Login</Link>
+          <Link to="/videocall">Video Call</Link>
         <Routes>
           <Route path="/" element={<Calendar />} />
           <Route path="/profile" element={ <Profile /> } />
           <Route path="/login" element={<Login />} />
+          <Route path="/videocall" element={<VideoCall />} />
         </Routes>
       </Router>
   );
---
+++ client/src/components/VideoCall.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoCall = () => {
+    return (
+        <div>
+            profile
+        </div>
+    );
+};
+
+export default VideoCall;
