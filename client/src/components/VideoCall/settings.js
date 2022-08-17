ff83307becb021df88c9c2d6af1ae438f025a4ce
Russell Elliott <russell@quadsquad.net>
2022-08-17 13:52:07 -0700

    env file initalized; id and token stored inside

---
+++ client/src/components/VideoCall/settings.js
@@ -0,0 +1,11 @@
+//Settings page to initalize Agora API stuff
+
+//Import the env file
+require('dotenv').config()
+
+//app id for the app
+const appId = process.env.ID
+
+//temporary token for the app (limited time usage)
+const token = process.env.TOKEN
+
