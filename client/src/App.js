645e2dcc320502d2e477339fb676d8dfdc5854e9
RaghuRaikar <96157136+RaghuRaikar@users.noreply.github.com>
2022-08-17 10:50:08 -0700

    Created Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,42 @@ import Calendar from "./components/Calendar";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
 import VideoCall from "./components/VideoCall";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+  
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
       <Router>
-          <Link to="/"> Home </Link>
-          <Link to="/profile"> Profile</Link>
-          <Link to="/login"> Login</Link>
-          <Link to="/videocall">Video Call</Link>
+        <nav>
+            <Link to="/"> Home </Link>
+            <Link to="/profile"> Profile</Link>
+            <Link to="/profile"> Profile</Link>
+            <Link to="/videocall">Video Call</Link>
+          {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      
+      </nav>
         <Routes>
           <Route path="/" element={<Calendar />} />
           <Route path="/profile" element={ <Profile /> } />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
           <Route path="/videocall" element={<VideoCall />} />
         </Routes>
       </Router>
