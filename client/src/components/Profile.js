5d638a518d1b8e00d0a6a15ee897eda19fe52416
Russell Elliott <russell@quadsquad.net>
2022-08-19 15:56:55 -0700

    added some dummy text

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const user = "user"
+const userEmail = "user@domain.com"
+const times = "times go here"
+
 const Profile = () => {
     return (
         <div>
-            profile
+            <h1>Profle</h1>
+            <p>Current user: {user}</p>
+            <p>Email: {userEmail}</p>
+            <h1>Scheduled Meetings</h1>
+            <p>You have meetings at these times:</p>
+            {times}
         </div>
     );
 };
