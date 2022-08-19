d4eeaa03d064c9699d9f40720cad028003ff98d3
Russell Elliott <russell@quadsquad.net>
2022-08-19 14:25:09 -0700

    new software for videocall; will update readme

--- client/src/components/VideoCall/VideoDashboard.js
+++ client/src/components/VideoCall/VideoDashboard.js
@@ -1,12 +1,23 @@
 import { useState } from "react";
-import Button from "@mui/material/Button"
-import VideoCall from "./VideoCall";
-
+//import Button from "@mui/material/Button"
+//import VideoCall from "./VideoCall";
+import VideoCallDisplay from "./VideoCallDisplay" //video call display component
 function App() {
-  const [inCall, setInCall] = useState(false);
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    //toggle shown state
+    setIsShown(current => !current);
+
+    //or simply set it to true
+    //setIsShown(true);
+  };
+
+  //const [inCall, setInCall] = useState(false);
 
   return (
-    <div className="App" style={{ height: "100%" }}>
+    /*<div className="App" style={{ height: "100%" }}>
       {inCall ? (
         <VideoCall setInCall={setInCall} />
       ) : (
@@ -18,6 +29,14 @@ function App() {
           Join Call
         </Button>
       )}
+    </div>*/
+    //At first, video div is not displayed.
+
+    <div className="App">
+      {/*Button only shows up if it hasn't been clicked yet.*/}
+      {!isShown && <button onClick={handleClick}>Click</button>}
+      {/*show component on click*/}
+      {isShown && <VideoCallDisplay />}
     </div>
   );
 }
